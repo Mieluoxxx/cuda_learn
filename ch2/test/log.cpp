0f3c6d9de30cf119c351b08d5b49ebe3784ea13d
Morgan Woods <weiyiding0@gmail.com>
2024-12-07 22:47:48 +0800

    添加测试模块

---
+++ ch2/test/log.cpp
@@ -0,0 +1,10 @@
+//
+// Created by moguw on 24-12-7.
+//
+
+#include <glog/logging.h>
+#include "log.h"
+
+void log_test() {
+    LOG(INFO) << "Test LOG";
+}
---
+++ ch2/test/log.h
@@ -0,0 +1,11 @@
+//
+// Created by moguw on 24-12-7.
+//
+
+#ifndef LOG_H
+#define LOG_H
+
+void log_test();
+
+
+#endif //LOG_H
