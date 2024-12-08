cce9ad8d695af8e95e57a1a92fdf25868733aa6c
Morgan Woods <weiyiding0@gmail.com>
2024-12-08 22:23:18 +0800

    朴素矩阵乘法算子

---
+++ ch3/project/src/op/interface.cpp
@@ -0,0 +1,31 @@
+#include "interface.h"
+
+#include <gpu/com_matrix_mul_op.cuh>
+
+namespace mop {
+    AddKernel get_add_op(mbase::DeviceType device_type) {
+        if (device_type == mbase::DeviceType::HOST) {
+            // 如果是 HOST，返回 nullptr，意味着没有使用 GPU
+            return nullptr;
+        } else if (device_type==mbase::DeviceType::Device) {
+            return cudaop::add_op_cu;
+        } else {
+            LOG(FATAL) << "Unknown device type for get a add kernel.";
+            return nullptr;
+        }
+    }
+
+    com_matrix_mul_op get_com_matrix_mul_op(mbase::DeviceType device_type) {
+        if (device_type == mbase::DeviceType::HOST) {
+            // 如果是 HOST，返回 nullptr，意味着没有使用 GPU
+            return nullptr;
+        } else if (device_type==mbase::DeviceType::Device) {
+            return cudaop::com_matrix_mul_op_cu;
+        } else {
+            LOG(FATAL) << "Unknown device type for get a add kernel.";
+            return nullptr;
+        }
+    }
+
+
+}
